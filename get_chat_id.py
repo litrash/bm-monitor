f693ee3b17534e13e3f8d69fd35a330b243fd404
litrash <128911713+litrash@users.noreply.github.com>
2026-08-20 23:33:22 +0800

    get chat id

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,5 +1,5 @@
 import requests, json
-r = requests.get('https://api.telegram.org/bot8854681783:AAGwaZP1QCLSbNc9j8I3W7G_iS1xUXbUW7I/getMe')
-print('BOT_INFO_START')
-print(r.text)
-print('BOT_INFO_END')
+r = requests.get('https://api.telegram.org/bot8854681783:AAGwaZP1QCLSbNc9j8I3W7G_iS1xUXbUW7I/getUpdates')
+print('TG_OUTPUT_START')
+print(r.text[:2000])
+print('TG_OUTPUT_END')
