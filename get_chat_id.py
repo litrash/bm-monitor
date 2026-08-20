8e9c81c13fc012d63a470d48eee85bdf9a2848f5
litrash <128911713+litrash@users.noreply.github.com>
2026-08-20 23:35:22 +0800

    check bot identity

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,5 +1,5 @@
 import requests, json
-r = requests.get('https://api.telegram.org/bot8854681783:AAGwaZP1QCLSbNc9j8I3W7G_iS1xUXbUW7I/getUpdates')
+r = requests.get('https://api.telegram.org/bot8854681783:AAGwaZP1QCLSbNc9j8I3W7G_iS1xUXbUW7I/getMe')
 print('TG_OUTPUT_START')
-print(r.text[:2000])
+print(r.text)
 print('TG_OUTPUT_END')
