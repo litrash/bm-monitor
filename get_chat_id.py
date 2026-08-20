7af837652a009ec76acf8aea04cd22ea1260194a
litrash <128911713+litrash@users.noreply.github.com>
2026-08-20 23:40:21 +0800

    fix: getUpdates script

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,5 +1,5 @@
-import requests, json
-r = requests.get('https://api.telegram.org/bot8854681783:AAGwaZP1QCLSbNc9j8I3W7G_iS1xUXbUW7I/getMe')
-print('TG_OUTPUT_START')
+import requests
+r = requests.get('https://api.telegram.org/bot8854681783:AAGwaZP1QCLSbNc9j8I3W7G_iS1xUXbUW7I/getUpdates')
+print('CHAT_START')
 print(r.text)
-print('TG_OUTPUT_END')
+print('CHAT_END')
