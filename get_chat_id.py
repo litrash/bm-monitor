4d58f765d8acf185a363a56c04153986c559c7e2
litrash <128911713+litrash@users.noreply.github.com>
2026-08-20 23:31:32 +0800

    get bot info

--- get_chat_id.py
+++ get_chat_id.py
@@ -1,11 +1,5 @@
 import requests, json
-r = requests.get('https://api.telegram.org/bot8854681783:AAGwaZP1QCLSbNc9j8I3W7G_iS1xUXbUW7I/getUpdates')
-data = r.json()
-print('CHAT_ID_START')
-for update in data.get('result', []):
-    msg = update.get('message', {})
-    chat = msg.get('chat', {})
-    print(f'chat_id: {chat.get("id")} | username: {chat.get("username", "")} | first_name: {chat.get("first_name", "")}')
-if not data.get('result'):
-    print('No messages found - did you send /start to the bot?')
-print('CHAT_ID_END')
+r = requests.get('https://api.telegram.org/bot8854681783:AAGwaZP1QCLSbNc9j8I3W7G_iS1xUXbUW7I/getMe')
+print('BOT_INFO_START')
+print(r.text)
+print('BOT_INFO_END')
